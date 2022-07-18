46d29841f23a4effecc1da2130d9807c85418705
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-07-17 19:06:17 -0700

    style meditate button

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,9 +5,11 @@ import "../styles/dashboard.css";
 function Dashboard() {
   return (
     <div className="dashboard-component">
-      <Link className="meditate-button" to="/timer">
-        MEDITATE
-      </Link>
+      <div className="button-wrapper">
+        <Link className="meditate-button" to="/timer">
+          MEDITATE
+        </Link>
+      </div>
     </div>
   );
 }
