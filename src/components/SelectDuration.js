04736c8cf8897253d6858c8beef3dc8c20950607
Nicholas Hibbits <nicholashibbits@gmail.com>
2022-07-20 15:22:57 -0700

    format time

--- src/components/SelectDuration.js
+++ src/components/SelectDuration.js
@@ -2,14 +2,14 @@ import "../styles/timer.css";
 function SelectDuration({ setTime }) {
   return (
     <main className="select-duration-component">
-      <form name="duration-range">
-        <label for="duration-range">Select Duration</label>
+      <form name="duration-range" className="duration-range">
+        <label htmlFor="duration-range">Select Duration</label>
         <input
           type="range"
           id="duration-input"
           onChange={(e) => setTime(e.target.value)}
           min="0"
-          max="60"
+          max="3600"
         ></input>
       </form>
     </main>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -7,6 +7,17 @@ import "../styles/timer.css";
 function Timer() {
   const [time, setTime] = useState(0);
 
+  function formatTime({ remainingTime }) {
+    const minutes = Math.floor(remainingTime / 60);
+    const seconds = remainingTime - minutes * 60;
+
+    if (remainingTime === 0) {
+      return "0";
+    }
+
+    return `${minutes}:${seconds}`;
+  }
+
   return (
     <div className="timer-component">
       <SelectDuration setTime={setTime} />
@@ -19,8 +30,10 @@ function Timer() {
         colors={["#004777", "#F7B801", "#A30000", "#A30000"]}
         colorsTime={[10, 6, 3, 0]}
         onComplete={() => ({ shouldRepeat: true, delay: 1 })}
+        children={formatTime}
       >
-        {({ remainingTime }) => remainingTime}
+        {/* {({ remainingTime }) => remainingTime} */}
+        {formatTime}
       </CountdownCircleTimer>
     </div>
   );
